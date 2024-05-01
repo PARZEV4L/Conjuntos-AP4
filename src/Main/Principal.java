aa89891bd9f03db525dae468ef58ca907a2444aa
Andrés Álvarez <andravid019@gmail.com>
2024-04-30 20:14:37 -0500

    menu primeros 5 puntos

--- src/Main/Principal.java
+++ src/Main/Principal.java
@@ -8,13 +8,8 @@ public class Principal {
 
 
     public static void main(String[] args) {
-      //  LocalDate x = FechaIngreso.fecha();
-            LDocentes ld = new LDocentes();
-  
-            ld.CargarDatos();
-            ld.mostrar(1);
-           // ld.conjuntoExclusivo(1);
-            ld.InterseccionCompletoCatedra();
+     Menu menu = new Menu();
+     menu.menu();
     }
     
 }
